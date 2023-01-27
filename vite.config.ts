b2f05911bafce5657f52c6c9365e225aa3460d4a
yerasyln <yerasyl.nurlanov@tarlanpayments.kz>
2023-01-27 14:42:27 +0600

    fix: aliasses config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
-import * as path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+        "@features": path.resolve(__dirname, "./src/features"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@lib":path.resolve(__dirname, "./src/lib"),
+        "@api":path.resolve(__dirname, "./src/api"),
+        "@ui":path.resolve(__dirname, "./src/ui"),
+        "@pages":path.resolve(__dirname, "./src/pages")
+
+      },
   },
 })
